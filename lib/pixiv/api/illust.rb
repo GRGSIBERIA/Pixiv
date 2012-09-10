f772c71e7ab520cf9a0dad782fceef31527dd41b
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-11 04:03:18 +0900

    add file

--- lib/pixiv/api/illust.rb
+++ lib/pixiv/api/illust.rb
@@ -1,3 +1,11 @@
 =begin
 特定のイラスト情報を問い合わせるためのクラス
 =end
+
+module Pixiv
+	module API
+		class Illust
+			
+		end
+	end
+end
---
+++ lib/pixiv/client.rb
@@ -0,0 +1,12 @@
+=begin
+Pixivにアクセスするためのクライアントクラス
+このクラスインスタンスを利用してPixivにアクセスする
+=end
+
+module Pixiv
+	class Client
+		def initialize
+			
+		end
+	end
+end
