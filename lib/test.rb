c9bd1151e148213c649ef5ccc290402204dd992a
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-12 04:01:19 +0900

    add test

--- lib/test.rb
+++ lib/test.rb
@@ -1,11 +1,35 @@
 require './pixiv.rb'
 
+def show(c, id)
+	i = c.illust.show(id)
+	puts i.class
+	puts i.title
+	puts i.caption
+	puts i.tags
+	puts i.date.to_s
+	if i.class == Pixiv::Presenter::Illust
+		puts i.size
+	end
+	puts i.tools
+	puts i.view_count
+	puts i.rated_count
+	puts i.score_count
+	puts i.location
+	puts i.extension
+	
+	puts i.thumbnail_uri
+	puts i.medium_uri
+	
+	if i.class == Pixiv::Presenter::Manga
+		puts i.page_count
+		puts i.large_uris
+		puts i.big_uris
+	end
+	
+	puts "------------"
+end
+
 c = Pixiv::Client.new
-i = c.illust.show(30042252)
-puts i.title
-puts i.caption
-puts i.tags
-puts i.date.to_s
-puts i.size
-puts i.tools
-puts i.view_count
+show(c, 30042252)
+show(c, 29908791)
+show(c, 28637532)
