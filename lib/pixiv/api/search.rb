d9aacc4470e251db9e460c820e3c9bb59735b2ea
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-20 05:40:05 +0900

    検索用クラスを追加

---
+++ lib/pixiv/api/search.rb
@@ -0,0 +1,15 @@
+﻿=begin
+検索用のAPI。
+主に検索フォームからタグやキーワードを入力する場合を想定。
+=end
+require './pixiv/api/base.rb'
+
+module Pixiv
+	module API
+		class Search < Base
+			def initialize(agent)
+				super(agent)
+			end
+		end
+	end
+end
