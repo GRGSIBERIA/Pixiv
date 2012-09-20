a415aef8029609e707c54c1e39a2e1f899ca0031
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-09-20 21:39:45 +0900

    パラメータの判定を簡単にする

--- lib/pixiv/api/search.rb
+++ lib/pixiv/api/search.rb
@@ -46,75 +46,98 @@ module Pixiv
 					uri += '&scd=' + param[:since_day].strftime("%Y-%m-%d")
 				end
 				
+				uri += MakePartialMatch(mode, param[:partial_match])
+				uri += MakeOrder(param[:order])
+				uri += MakePictureSizeRange(param[:size])
+				uri += MakeAspectRatio(param[:ratio])
+			end
+			
+			# 並び順を生成する、orderに何か入ってると過去順
+			def MakeOrder(order)
+				if order != nil then
+					"&order=date"
+				else
+					""
+				end
+			end
+			
+			# 部分一致と完全一致の判定
+			def MakePartialMatch(mode, partial_match)
 				case mode
 				when "keyword" then
 					# キーワードの場合は強制
-					uri += '&s_mode=s_tc'
+					'&s_mode=s_tc'
 				when "tag" then
 					# 部分一致の場合のみ付ける
-					uri += param[:partial_match] != nil ? '&s_mode=s_tag' : ""	
+					partial_match != nil ? '&s_mode=s_tag' : ""	
+				else
+					""
 				end
-				
-				uri += param[:order] != nil ? '&order=' + param[:order] : '&order=date_d'
-				uri += param[:size] != nil ? MakePictureSizeRange(param[:size]) : ""
-				uri += param[:ratio] != nil ? MakeAspectRatio(param[:ratio]) : ""
 			end
 			
 			# アスペクト比の設定
 			def MakeAspectRatio(ratio)
-				if ratio.class == String then
-					case ratio
-					when "horizontal"	# 横長
-						ratio = "0.5"
-					when "vertical"	# 縦長
-						ratio = "-0.5"
-					when "rectangle" # 正方形
-						ratio = "0"
+				if ratio != nil then
+					if ratio.class == String then
+						case ratio
+						when "horizontal"	# 横長
+							ratio = "0.5"
+						when "vertical"	# 縦長
+							ratio = "-0.5"
+						when "rectangle" # 正方形
+							ratio = "0"
+						end
+					elsif ratio.class != Float then
+						ratio = ratio.to_s
+					else
+						raise ArgumentError, "型がおかしい:" + ratio.class
 					end
-				elsif ratio.class != Float then
-					ratio = ratio.to_s
+					"&ratio=" + ratio
 				else
-					raise ArgumentError, "型がおかしい:" + ratio.class
+					""
 				end
-				"&ratio=" + ratio
 			end
 			
 			# 画像の大きさを指定してその範囲の画像を取得する
 			# ngtはn以下、nltはn以上の意味
 			def MakePictureSizeRange(size)
-				wgt = nil; hgt = nil; wlt = nil; hlt = nil
+				if size != nil then
+					wgt = nil; hgt = nil; wlt = nil; hlt = nil
 
-				if size.class == String then
-					# 画像のサイズを文字列で指定
-					case size
-					when "small" then
-						wgt = 1000
-						hgt = 1000
-					when "middle" then
-						wgt = 3000
-						hgt = 3000
-						wlt = 1001
-						hlt = 1001
-					when "large" then
-						wlt = 3001
-						hlt = 3001
-					else
-						raise ArgumentError, "small, middle, largeのどれかを指定してください"
+					if size.class == String then
+						# 画像のサイズを文字列で指定
+						case size
+						when "small" then
+							wgt = 1000
+							hgt = 1000
+						when "middle" then
+							wgt = 3000
+							hgt = 3000
+							wlt = 1001
+							hlt = 1001
+						when "large" then
+							wlt = 3001
+							hlt = 3001
+						else
+							raise ArgumentError, "small, middle, largeのどれかを指定してください"
+						end
+					elsif size.class == Hash then
+						# 数値で直接指定
+						wgt = size[:wgt]
+						hgt = size[:hgt]
+						wlt = size[:wlt]
+						hlt = size[:hlt]
 					end
-				elsif size.class == Hash then
-					# 数値で直接指定
-					wgt = size[:wgt]
-					hgt = size[:hgt]
-					wlt = size[:wlt]
-					hlt = size[:hlt]
+					
+					parameters = ""	# ない奴は無視する
+					if wgt != nil then parameters += '&wgt=' + wgt.to_s; end;
+					if hgt != nil then parameters += '&hgt=' + hgt.to_s; end;
+					if wlt != nil then parameters += '&wlt=' + wlt.to_s; end;
+					if hlt != nil then parameters += '&hlt=' + hlt.to_s; end;
+					parameters
+				else
+					""
 				end
-				
-				parameters = ""	# ない奴は無視する
-				if wgt != nil then parameters += '&wgt=' + wgt.to_s; end;
-				if hgt != nil then parameters += '&hgt=' + hgt.to_s; end;
-				if wlt != nil then parameters += '&wlt=' + wlt.to_s; end;
-				if hlt != nil then parameters += '&hlt=' + hlt.to_s; end;
-				parameters
 			end
 			
 			# キーワードを結合してエンコードする
