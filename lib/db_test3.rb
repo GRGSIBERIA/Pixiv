5cd4b9106e75c50c55d20e4dcc7eef98f57ccfda
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-10-25 23:42:42 +0900

    ユーザIDとイラストIDのひもづけ

--- lib/db_test3.rb
+++ lib/db_test3.rb
@@ -1,10 +1,10 @@
 require './pixiv/database/db.rb'
 
 db = Pixiv::Database::DB.new
-table = Pixiv::Database::Tables::TagTable.new(db.db)
+table = Pixiv::Database::Tables::IllustInfoTable.new(db.db)
 
 st = Time.now
-result = table.count
+result = table.GetUserIDFromIllustID(20)
 et = Time.now
 puts (et - st).to_s
 
--- lib/pixiv/database/Tables/illust_info_table.rb
+++ lib/pixiv/database/Tables/illust_info_table.rb
@@ -1,18 +1,30 @@
 =begin
 イラスト情報のテーブルクラス  
 =end
+require './pixiv/database/tables/table_base.rb'
 
 module Pixiv
   module Database
     module Tables
-      class IllustInfoTable
+      class IllustInfoTable < TableBase
         def initialize(db)
           super(db)
         end
         
+        # ユーザIDからイラストを取得する
+        # @param userid [Int] ユーザID
+        # @return [Array<Int>] イラストIDの配列
         def GetIllustIDsFromUserID(userid)
           sql = 'select illust_id from illust_info_table where userid = ?'
           GetMulti(sql, [userid], "i")
+        end
+        
+        # イラストIDからユーザIDを引き出す
+        # @param illust_id [Int] イラストID
+        # @return [Int] ユーザID
+        def GetUserIDFromIllustID(illust_id)
+          sql = 'select userid from illust_info_table where illust_id = ? limit 1'
+          GetSingle(sql, [illust_id], "i")
         end
       end
     end
