2c2d8361531eb62eb183ee86dfbdac9370c86ad9
Eiichi Takebuchi <nanashi4129@gmail.com>
2012-10-29 03:07:28 +0900

    確率までは出した

--- lib/pixiv/database/clastering/guess_tag_type.rb
+++ lib/pixiv/database/clastering/guess_tag_type.rb
@@ -12,10 +12,6 @@ module Pixiv
         end
         
         # @param illust_id [Int] 推測させたいイラストID
-        # @return [Array<Hash>] タグごとの作品やキャラクターなどの推測
-        # @return [Array<Hash>] :tagid タグID
-        # @return [Array<Hash>] :tagname タグの名前
-        # @return [Array<Hash>] :type :work, :character, :property
         def GuessType(illust_id)
           page_limit = 5  # 1ページに存在するイラストの数
           limit_norm = 1.0 / page_limit.to_f
@@ -45,9 +41,8 @@ module Pixiv
             end
             
             # ページ単位で残ったタグを調べる
-            probs_buffer_from_page = Array.new
+            tag_counter = Hash.new  # 見つかったタグを調べていく
             tags_by_searched.each do |illust, tags|
-              tag_counter = Hash.new  # 見つかったタグを調べていく
               target_tag.each do |counting_tag|
                 # 対象のタグが検索イラストのタグの中に見つかればカウントする
                 if tags.include?(counting_tag) and counting_tag != target_tag then
@@ -55,21 +50,19 @@ module Pixiv
                   tag_counter[counting_tag] += 1
                 end
               end
-              
-              # カウントを確率に戻す
-              tag_counter.each do |tag, count|
-                tag_counter[tag] = count.to_f * limit_norm.to_f # イラストごとのタグの確率を求める
-              end
-              probs_buffer_from_page << tag_counter
-              # ---ここまでがページ単位---
             end
             
-            probs[target_tag] = probs_buffer_from_page  # ページ単位の確率をまとめる
+            # カウントを確率に戻す
+            tag_counter.each do |tag, count|
+              tag_counter[tag] = count.to_f * limit_norm.to_f # イラストごとのタグの確率を求める
+            end
+            probs[target_tag] = tag_counter # タグごとの似たタグの確率
           end
-          # @return probs 対象タグごとにページ単位で検索されたイラストの対象タグが含まれている確率が入ってる
           
-          # probsでタグ->ページ->イラストの確率を求めた
-          # ページごとの確率にまとめる
+          # probs => Hash<Int => Hash<Int => Float>>
+          # probs => Hash<tagid => Hash<tagid => probability>>
+          # {A => {B => 0.8, C => 0.6}, B => {A => 1.0}}
+          probs
         end
       end
     end
